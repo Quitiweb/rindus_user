2737ccfee808782efcb9f9e74918d5f2c10dd26e
Quitiweb <rafa@quitiweb.com>
2021-12-14 22:05:38 +0100

    Add owner field to user_crud model
    This field will reference which staff or superuser has created it.
    - If it is a superuser, it will be able to view and edit the owner of these user_cruds
    - If it is a staff user, it won't be able to view or edit the owner and it will be able only to manage it's own user_cruds

--- user_app/admin.py
+++ user_app/admin.py
@@ -5,4 +5,33 @@ from user_app import models
 
 @admin.register(models.UserCrud)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'iban')
+    fields = ('first_name', 'last_name', 'iban')
+    list_display = ('first_name', 'last_name', 'iban')
+
+    def get_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.fields + ('owner', )
+
+        return self.fields
+
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return self.list_display + ('owner', )
+
+        return self.list_display
+
+    def get_queryset(self, request):
+        """
+            If is_superuser: returns everything
+            if is_staff: returns only crud_users created by the staff user
+        """
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        else:
+            return queryset.filter(owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            obj.owner = request.user
+        super().save_model(request, obj, form, change)
--- user_app/models.py
+++ user_app/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 
-# Create your models here.
 class UserCrud(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     iban = models.CharField(max_length=50)
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='user_crud', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.first_name
