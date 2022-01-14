f18047ed45e17bfd07052e35dce48549e61ac549
Quitiweb <rafa@quitiweb.com>
2022-01-14 01:39:19 +0100

    update post views to create the new user record

--- user_app/views.py
+++ user_app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
 from user_app.forms import CustomUserCreationForm
+from user_app.models import UserCrud
 
 
 def basic_crud_view(request):
@@ -10,6 +11,17 @@ def basic_crud_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            iban = form.cleaned_data['iban']
+
+            UserCrud.objects.create(
+                owner=request.user,
+                first_name=first_name,
+                last_name=last_name,
+                iban=iban
+            )
+
             return HttpResponseRedirect('/thanks/')
 
     else:
